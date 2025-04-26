7bd347f7a87fef776e916175415a874750498309
Rodrigo Eustáquio Duque <rodrigoeduque@hotmail.com>
2025-04-25 21:55:33 -0300

    feat: projeto criado

---
+++ src/main/java/br/com/rodrigoeduque/meu_portifolio/MeuPortifolioApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoeduque.meu_portifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPortifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPortifolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodrigoeduque/meu_portifolio/MeuPortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigoeduque.meu_portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
